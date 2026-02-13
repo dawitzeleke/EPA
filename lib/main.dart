ca00a913bbb046c9f16a69db8879af5a5c9c9ba0
dawitzeleke <dawitzeleke92@gmail.com>
2026-02-13 12:11:05 +0300

    Set up the language in the main file

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 import 'core/di/injection_container.dart' as di;
+import 'core/constants/languages/app_translations.dart';
+import 'app/modules/language/controllers/language_controller.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,9 +13,14 @@ void main() async {
   // Initialize dependency injection
   await di.InjectionContainer.init();
 
+  final languageController = Get.put(LanguageController(), permanent: true);
+
   runApp(
     GetMaterialApp(
       title: "EPA",
+      translations: AppTranslations(),
+      locale: languageController.locale.value,
+      fallbackLocale: AppTranslations.fallbackLocale,
       initialRoute: Routes.SPLASH,
       getPages: AppPages.routes,
       debugShowCheckedModeBanner: false,
